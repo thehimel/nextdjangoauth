2f252ebfb39b9089faf64da1f0a7264b5ccb0ab4
Himel Das <60639301+thehimel@users.noreply.github.com>
2025-03-02 16:49:05 +0100

    feat(auth): implement service for requesting magic link

---
+++ modules/auth/messages.ts
@@ -0,0 +1,4 @@
+export const authMessages = {
+  REQUEST_MAGIC_LINK_SUCCESS: "To continue, click the link sent to",
+  REQUEST_MAGIC_LINK_ERROR: "Something went wrong while requesting the verification link. Please try again.",
+};
