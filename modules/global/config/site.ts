b8a44f372854b3f0f9aaa868f280af7e7207786c
Himel Das <60639301+thehimel@users.noreply.github.com>
2025-03-08 14:13:10 +0100

    chore: remove 'Pricing' page and nav item from site configuration

--- modules/global/config/site.ts
+++ modules/global/config/site.ts
@@ -16,10 +16,6 @@ export const siteConfig = {
       href: "/docs",
     },
     {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
       label: "Blog",
       href: "/blog",
     },
