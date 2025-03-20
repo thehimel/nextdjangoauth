b4ec4466bcf124c31af5b5b6198609003b238aa8
Himel Das <60639301+thehimel@users.noreply.github.com>
2025-03-20 03:26:41 +0100

    Add FRONTEND_URL in the global env CONFIG

--- modules/global/config/env.ts
+++ modules/global/config/env.ts
@@ -2,4 +2,5 @@ export const CONFIG = {
   SITE_NAME: process.env.NEXT_PUBLIC_SITE_NAME || "SiteName",
   SITE_SHORTNAME: process.env.NEXT_PUBLIC_SITE_SHORTNAME || "SN",
   DJANGO_BACKEND_URL: process.env.DJANGO_BACKEND_URL || "http://localhost:8000",
+  FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000",
 };
