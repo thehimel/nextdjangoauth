29ae2b1a7378d7b1e42c86c91a8615dea4b7c8b7
Himel Das <60639301+thehimel@users.noreply.github.com>
2025-03-21 03:44:55 +0100

    i18n: add TypeScript augmentation

---
+++ i18n/types/global.d.ts
@@ -0,0 +1,12 @@
+import messages from "../../messages/en.json";
+
+import { routing } from "@/i18n/routing";
+import { formats } from "@/i18n/request";
+
+declare module "next-intl" {
+  interface AppConfig {
+    Locale: (typeof routing.locales)[number];
+    Messages: typeof messages;
+    Formats: typeof formats;
+  }
+}
