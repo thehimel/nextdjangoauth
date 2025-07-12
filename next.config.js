7f90e558227de5ec47c4a10f0a54df783cef9808
Himel Das <60639301+thehimel@users.noreply.github.com>
2025-07-12 12:51:52 +0200

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,4 +4,11 @@ const nextConfig = {};
 const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
 
-module.exports = withNextIntl(nextConfig);
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  // Add Nextra-specific options here
+})
+
+// Combine both plugins
+module.exports = withNextIntl(withNextra(nextConfig));
